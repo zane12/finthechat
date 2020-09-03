599c609b8be15d2e712773ad33b5eb509e47bd7b
Zane Armstrong <zane.armstrong@comcast.net>
2020-09-03 18:17:30 -0500

    Added betting functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import pymongo
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 
@@ -13,20 +14,22 @@ GUILD = os.getenv('DISCORD_GUILD')
 myclient = pymongo.MongoClient(DBSERVER)
 
 mydb = myclient["finthechat"]
-mycol = mydb["users"]
 
-client = discord.Client()
+userCol = mydb["users"]
+betCol = mydb["bets"]
+
+bot = commands.Bot(command_prefix='!')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
 
-    print(f'{client.user} has connected to the following server:\n'
+    print(f'{bot.user} has connected to the following server:\n'
           f'{guild.name}(id: {guild.id})')
 
 
-@client.event
+@bot.event
 async def on_message(message):
 
     if message.author.bot:
@@ -34,33 +37,124 @@ async def on_message(message):
 
     if message.content == 'f' or message.content == 'F':
 
-        user = mycol.find_one({"user": message.author.display_name})
+        user = userCol.find_one({"user": message.author.id})
 
         if user == None:
-            mycol.insert_one({"user": message.author.display_name, "score": 1})
+            userCol.insert_one({"user": message.author.id, "score": 1})
         else:
-            mycol.update_one({"user": message.author.display_name}, {
-                             "$set": {"score": user["score"] + 1}})
+            userCol.update_one({"user": message.author.id}, {
+                "$set": {"score": user["score"] + 1}})
 
         await message.channel.send("F")
+    await bot.process_commands(message)
 
-    if message.content == '!leaderboard':
-        leaderboard = mycol.find().sort("score", -1)
-        leaderString = ''
-        i = 0
-        for x in leaderboard:
-            i += 1
-            if i == 10:
-                break
-            if i == 1:
-                leaderString += "The reigning champ "
-            timeString = "time"
-            if x["score"] > 1:
-                timeString = "times"
-            leaderString += x["user"] + " has paid respects " + \
+
+@bot.command()
+async def leaderboard(ctx):
+
+    leaderboard = userCol.find().sort("score", -1)
+
+    leaderString = ''
+
+    i = 0
+
+    for x in leaderboard:
+        i += 1
+        if i == 10:
+            break
+
+        print("{}".format(x))
+
+        if i == 1:
+            leaderString += "The reigning champ "
+        timeString = "time"
+
+        user = await bot.fetch_user(x["user"])
+
+        if x["score"] > 1:
+            timeString = "times"
+            leaderString += user.display_name + " has paid respects " + \
                 str(x["score"]) + " " + timeString + "\n"
 
-        await message.channel.send(leaderString)
+        await ctx.send(leaderString)
 
 
-client.run(TOKEN)
+@bot.command()
+async def bet(ctx, user: discord.Member, amt: int):
+    check = betCol.find()
+
+    found = False
+    for x in check:
+        if ctx.author.id and user.id in x["users"]:
+            found = True
+
+    if found:
+        await ctx.send("You may only have one active bet with {} at a time".format(user.display_name))
+        return
+
+    betCol.insert_one(
+        {"users": [ctx.author.id, user.id], "amt": amt, "winner": None, "loser": None})
+
+    await ctx.send('{} bet {} {} F\'s'.format(ctx.author.display_name, user.display_name, amt))
+
+
+@bot.command()
+async def winBet(ctx, user: discord.Member):
+    check = betCol.find()
+
+    found = False
+    for x in check:
+        if ctx.author.id and user.id in x["users"]:
+            found = True
+            bet = x
+
+    if(found):
+        betCol.find_one_and_update(bet, {"$set": {"winner": ctx.author.id}})
+    else:
+        await ctx.send("You don't have an active bet with {}".format(user.display_name))
+        return
+
+    if bet["loser"] == user.id:
+        amt = bet["amt"]
+        userCol.find_one_and_update({"user": ctx.author.id}, {
+            "$inc": {"score": amt}})
+        userCol.find_one_and_update({"user": user.id}, {
+            "$inc": {"score": (0 - amt)}})
+        winner = userCol.find_one({"user": ctx.author.id})
+        loser = userCol.find_one({"user": user.id})
+        betCol.find_one_and_delete({"_id": bet["_id"]})
+        await ctx.send("{} now has {} F's \n {} now has {} F's".format(ctx.author.display_name, winner["score"], user.display_name, loser["score"]))
+
+    else:
+        await ctx.send("Waiting for {} to confirm the bet".format(user.display_name))
+
+
+@ bot.command()
+async def loseBet(ctx, user: discord.Member):
+    check = betCol.find()
+
+    found = False
+    for x in check:
+        if ctx.author.id and user.id in x["users"]:
+            found = True
+            bet = x
+
+    if(found):
+        betCol.find_one_and_update(bet, {"$set": {"loser": ctx.author.id}})
+    else:
+        await ctx.send("You don't have an active bet with {}".format(user.display_name))
+        return
+
+    if bet["winner"] == user.id:
+        amt = bet["amt"]
+        userCol.find_one_and_update({"user": ctx.author.id}, {
+            "$inc": {"score": (0 - amt)}})
+        userCol.find_one_and_update({"user": user.id}, {
+            "$inc": {"score": (amt)}})
+        winner = userCol.find_one({"user": user.id})
+        loser = userCol.find_one({"user": ctx.author.id})
+        betCol.find_one_and_delete({"_id": bet["_id"]})
+
+        await ctx.send("{} now has {} F's \n {} now has {} F's".format(ctx.author.display_name, loser["score"], user.display_name, winner["score"]))
+
+bot.run(TOKEN)
